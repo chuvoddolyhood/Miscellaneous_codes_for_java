d5a9cf9563251d3f0367e792646c191457a20f71
Tran Nhan Nghia <63031237+chuvoddolyhood@users.noreply.github.com>
2021-01-21 10:18:59 +0700

    Add files via upload

---
+++ AccessWeb.java
@@ -0,0 +1,9 @@
+package linhtinh;
+
+public class AccessWeb {
+	    public static void main(String[] args) throws Exception {
+	        Runtime run = Runtime.getRuntime();
+	        String url = "https://chuvoddolyhood.github.io/personal/";
+	        run.exec("rundll32 url.dll, FileProtocolHandler " + url);
+	    }
+}
